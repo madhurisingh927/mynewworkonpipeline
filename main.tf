a9f012ed6ae5ec0a763b1f7cdfb4d9f90e686c89
Madhuri Singhn <singmadhurri@gmail.com>
2025-08-02 15:23:07 +0530

    rg transfer

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm ={
+        source = "hashicorop/azurerm"
+        version = "4.35.0"
+
+    }
+  }
+}
+provider "azurerm" {
+  features{}
+}
+resource "azurerm_resource_group" "name" {
+  name = "mygit23"
+  location = "westus"
+}
